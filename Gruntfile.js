fbff61f48dbe33f203344ac07be07963b25b790f
liuhuawei0719 <liuhuawei0719@126.com>
2016-08-21 18:58:22 +0800

    Add all

---
+++ Gruntfile.js
@@ -0,0 +1,123 @@
+/**
+ * Created by Hughie Liu on 2016/8/4.
+ */
+
+//包装函数
+module.exports = function(grunt) {
+    //插件配置
+    grunt.initConfig({
+        //获取package.json信息
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['dist/js/**.js'],
+                dest: 'dist/js/<%= pkg.name %>-full.js'
+            }
+        },
+
+        //uglify
+        uglify: {
+            buildall: {
+                options: {
+                    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - by <%= pkg.author %> ' +
+                    '<%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                    mangle: {
+                        except: ['jQuery']
+                    },
+                    compress: {
+                        drop_console: true
+                    },
+                    // object properties mangling(将非浏览器内置对象名进行混淆)
+                    // mangleProperties: true,
+                    // reserveDOMCache: true,
+                    //压缩美化
+                    // beautify: {
+                    //     width: 10,
+                    //     beautify: true
+                    // },
+                    report: "gzip"
+                },
+
+                //Compiling all files in a folder dynamically
+                files: [{
+                    expand:true,
+                    cwd: 'src',
+                    src: 'js/*.js',
+                    dest:'dist'
+                }]
+              }
+             },
+
+        //jshint
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            options: {
+                jshintrc:'.jshintrc',
+                //这里是覆盖JSHint默认配置的选项
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        },
+
+        //watch
+        watch: {
+            scripts: {
+                files: [
+                    'src/{,*/}*.css',
+                    'src/{,*/}*.js',
+                    'src/{,*/}*.html'
+                ],
+                tasks: ['default'],
+                options:{
+                    spawn: true,
+                    interrupt: true
+                }
+            }
+        },
+
+        //copy
+        copy: {
+            main: {
+                expand: true,
+                src: 'src/*',
+                dest: 'dist/',
+                flatten: true,
+                filter: 'isFile'
+            }
+        },
+
+        //cssmin
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'dist/css',
+                    ext: '.min.css'
+                }]
+            }
+        }
+    });
+
+    //加载npm任务
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    //注册任务
+    grunt.registerTask('minall',['uglify:buildall']);
+    // grunt.registerTask('test', ['jshint', 'qunit']);
+    grunt.registerTask('default', ['jshint','cssmin','copy','concat','uglify','watch']);
+
+};
---
+++ src/js/main.js
@@ -0,0 +1,72 @@
+$(function(){
+    $('#chrome').click(function () {
+        $('.internet').addClass('open');
+        if ($('.settings').hasClass('open') == true) {
+            $('.settings').addClass('bottom');
+            $('.settings').click(function() {
+                $('.internet').addClass('bottom');
+                $('.settings').removeClass('bottom');
+            });
+        }
+    });
+    $('#close-internet').click(function () {
+        $('.internet').removeClass('open');
+        if ($('.settings').hasClass('open') == true) {
+            $('.settings').removeClass('bottom');
+        }
+    });
+// if($('.settings').hasClass('open') == true && $('.internet').hasClass('open') == true){
+    $('.internet').click(function() {
+        // $('.internet').removeClass('bottom').addClass('open');
+        // $('.settings').addClass('bottom');
+        $('.settings').addClass('bottom');
+    });
+// }
+    $('#settings').click(function () {
+        $('.settings').addClass('open');
+        if ($('.internet').hasClass('open') == true) {
+            $('.internet').addClass('bottom');
+        }
+    });
+    $('#close-settings').click(function () {
+        $('.settings').removeClass('open');
+        if ($('.internet').hasClass('open') == true) {
+            $('.internet').removeClass('bottom');
+        }
+    });
+
+    $('div.imag').click(function () {
+        var imag = $(this);
+        $('.imag').children().removeClass('chosen');
+        imag.children().addClass('chosen');
+        var url = 'url(' + imag.children().attr('src') + ') no-repeat center center fixed';
+
+        $('body').css({ 'background': url, 'background-size': 'cover' });
+    });
+
+    $('.slider').click(function () {
+        $(this).toggleClass('slider-active');
+    });
+
+    $('#dock-hidden-slider').click(function () {
+        $('.dock').toggleClass('hidden');
+    });
+
+    $('.dock-position-slider').click(function () {
+        var pos = $(this).attr('id');
+        $('.dock-position-slider').removeClass('slider-active');
+        $(this).addClass('slider-active');
+        if ($('.dock').hasClass('bottom') == true) {
+            $('.dock').removeClass('bottom');
+        } else if ($('.dock').hasClass('left') == true) {
+            $('.dock').removeClass('left');
+        } else if ($('.dock').hasClass('right') == true) {
+            $('.dock').removeClass('right');
+        }
+
+        $('.dock').addClass(pos);
+    });
+
+});
+
+
